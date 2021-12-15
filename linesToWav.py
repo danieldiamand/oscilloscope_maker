2419338ef5cd6060fe3d6e45470c9cc5f4ae394b
danieldiamand_PC <daniel@diamand.org>
2021-12-15 17:10:05 +0000

    Removed coordinateToWav now obsolete, improved comments

--- linesToWav.py
+++ linesToWav.py
@@ -1,8 +1,9 @@
-import wave, math, struct
+import wave, struct
 
-sampleRate = 44100
-loops = 40
-scale = 0.25
+frameRate = 44100
+fileName = "test"
+loops = 1 #how many loops it runs for
+scale = 1
 
 class Line:
     def __init__(self, x1,y1,x2,y2, drawFrames=100):
@@ -10,27 +11,24 @@ class Line:
         self.y1 = y1*scale
         self.x2 = x2*scale
         self.y2 = y2*scale
-        self.drawFrames = drawFrames
+        self.drawFrames = drawFrames #how many frames it takes to draw line, ie how long it takes to move between point 1 and 2 
         self.xDif = (self.x2-self.x1)/self.drawFrames
         self.yDif = (self.y2-self.y1)/self.drawFrames
 
-lineArray = [Line(0,-1,1,0), Line(1,0,0,1), Line(0,1,-1,0), Line(-1,0,0,-1)] #each object represents a line made of x1,y1,x2,y2 scaled by scale
+lineArray = [Line(0,-1,1,0), Line(1,0,0,1), Line(0,1,-1,0), Line(-1,0,0,-1)] #each object represents a line betweeb two coordinates x1,y1,x2,y2
 
-leftChannel = [
-]
-rightChannel = [
-    ]
-
+leftChannel = []
+rightChannel = []
 for _ in range(loops):
     for line in (lineArray):
         for i in range(line.drawFrames):
             rightChannel.append(line.x1+(line.xDif)*i)
             leftChannel.append(line.y1+(line.yDif)*i)
 
-with wave.open("test.wav", "w") as f:
+with wave.open(fileName+".wav", "w") as f:
     f.setnchannels(2)
     f.setsampwidth(2)
-    f.setframerate(sampleRate)
+    f.setframerate(frameRate)
     for samples in zip(leftChannel, rightChannel):
         for sample in samples:
             sample = int(sample * (2 ** 15 - 1))
