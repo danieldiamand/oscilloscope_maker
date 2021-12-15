a9dc96d01f98f46fa184fed3d20388d57949a454
danieldiamand_PC <daniel@diamand.org>
2021-12-15 16:39:30 +0000

    added drawFrames to class alowing for different line times

--- linesToWav.py
+++ linesToWav.py
@@ -1,32 +1,31 @@
 import wave, math, struct
 
 sampleRate = 44100
-loopValue = 441 #will draw shape (sampleRate/loopValue) number of times
+loops = 40
 scale = 0.25
 
 class Line:
-    def __init__(self, x1,y1,x2,y2):
+    def __init__(self, x1,y1,x2,y2, drawFrames=100):
         self.x1 = x1*scale
         self.y1 = y1*scale
         self.x2 = x2*scale
         self.y2 = y2*scale
-        self.xDif = (self.x2-self.x1)
-        self.yDif = (self.y2-self.y1)
+        self.drawFrames = drawFrames
+        self.xDif = (self.x2-self.x1)/self.drawFrames
+        self.yDif = (self.y2-self.y1)/self.drawFrames
 
 lineArray = [Line(0,-1,1,0), Line(1,0,0,1), Line(0,1,-1,0), Line(-1,0,0,-1)] #each object represents a line made of x1,y1,x2,y2 scaled by scale
-loopRate = int(sampleRate/loopValue)
-actionRate = int(loopRate/len(lineArray))
 
 leftChannel = [
 ]
 rightChannel = [
     ]
 
-for _ in range(loopRate):
+for _ in range(loops):
     for line in (lineArray):
-        for i in range(actionRate):
-            rightChannel.append(line.x1+(line.xDif/actionRate)*i)
-            leftChannel.append(line.y1+(line.yDif/actionRate)*i)
+        for i in range(line.drawFrames):
+            rightChannel.append(line.x1+(line.xDif)*i)
+            leftChannel.append(line.y1+(line.yDif)*i)
 
 with wave.open("test.wav", "w") as f:
     f.setnchannels(2)
