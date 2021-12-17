a4522a8b087f12dfcd9d18a367ff24e8097d294f
danieldiamand_PC <daniel@diamand.org>
2021-12-17 14:07:42 +0000

    Added speedLimit so line draws at aprox same speed at all times

--- objToWav.py
+++ objToWav.py
@@ -1,27 +1,31 @@
-import wave, struct, readObj, rotateVertex
+import wave, struct, math
+from readObj import readObj
+from rotateVertex import rotateVertex
 
-objFileName = "house"
-saveFileName = "house"
+objFileName = "cube"
+saveFileName = "cube"
 frameRate = 44100
 initRotation = [0,0,-1.5708]
-rotationArray = [0.0174533,0,0] #in radians
+rotationArray = [0,0,0] #in radians
 pausePerRotate = 1 #in frames
 rotations = 360
 loops = 1 #how many loops it runs for
 scale = 0.75
+speedLimit = 0.02
 
 class Line:
-    def __init__(self, x1,y1,x2,y2, drawFrames=5):
+    def __init__(self, x1,y1,x2,y2, speedLimit=0.025):
         self.x1 = x1*scale
         self.y1 = y1*scale
         self.x2 = x2*scale
         self.y2 = y2*scale
-        self.drawFrames = drawFrames #how many frames it takes to draw line, ie how long it takes to move between point 1 and 2 
-        self.xDif = (self.x2-self.x1)/self.drawFrames
-        self.yDif = (self.y2-self.y1)/self.drawFrames
+        self.drawFrames = int(math.hypot(self.x2-self.x1, self.y2-self.y1)/speedLimit) #how many frames it takes to draw line, ie how long it takes to move between point 1 and 2 
+        self.xStep = (self.x2-self.x1)/self.drawFrames
+        self.yStep = (self.y2-self.y1)/self.drawFrames
 
-vertices, edges = readObj.readObj(objFileName)
-vertices = [rotateVertex.rotateVertex(vertex, initRotation) for vertex in vertices]
+
+vertices, edges = readObj(objFileName)
+vertices = [rotateVertex(vertex, initRotation) for vertex in vertices]
 lineArray = []
 
 
@@ -30,17 +34,16 @@ rightChannel = []
 for m in range(rotations):
     lineArray = []
     for edge in edges:
-        lineArray.append(Line(vertices[edge[0]][0],vertices[edge[0]][1], vertices[edge[1]][0], vertices[edge[1]][1],50))
+        lineArray.append(Line(vertices[edge[0]][0],vertices[edge[0]][1], vertices[edge[1]][0], vertices[edge[1]][1]))
     for _ in range(pausePerRotate):
         for line in (lineArray):
             for i in range(line.drawFrames):
-                rightChannel.append(line.x1+(line.xDif*i))
-                leftChannel.append(line.y1+(line.yDif*i))
+                rightChannel.append(line.x1+(line.xStep*i))
+                leftChannel.append(line.y1+(line.yStep*i))
     
-    vertices = [rotateVertex.rotateVertex(vertex, rotationArray) for vertex in vertices]
-    print("rotation: "+str(m))
+    vertices = [rotateVertex(vertex, rotationArray) for vertex in vertices]
+ 
 
-print(max(rightChannel))
 with wave.open(saveFileName+".wav", "w") as f:
     f.setnchannels(2)
     f.setsampwidth(2)
