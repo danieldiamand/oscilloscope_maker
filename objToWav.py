757221970a8217f8279dee46df47031935c1cca2
danieldiamand_PC <daniel@diamand.org>
2021-12-19 18:20:08 +0000

    Stores to file as data is generated

--- objToWav.py
+++ objToWav.py
@@ -1,6 +1,7 @@
 import wave, struct, math
 from readObj import readObj
-from rotateVertex import rotateVertex
+from rotateVertex import rotateVerticies
+
 
 objFileName = "cube"
 saveFileName = "cube"
@@ -10,11 +11,11 @@ rotationArray = [0,0,0] #in radians
 pausePerRotate = 1 #in frames
 rotations = 360
 loops = 1 #how many loops it runs for
-scale = 0.75
+scale = 0.3
 speedLimit = 0.02
 
 class Line:
-    def __init__(self, x1,y1,x2,y2, speedLimit=0.025):
+    def __init__(self, x1,y1,x2,y2, speed = 20):
         self.x1 = x1*scale
         self.y1 = y1*scale
         self.x2 = x2*scale
@@ -22,33 +23,26 @@ class Line:
         self.drawFrames = int(math.hypot(self.x2-self.x1, self.y2-self.y1)/speedLimit) #how many frames it takes to draw line, ie how long it takes to move between point 1 and 2 
         self.xStep = (self.x2-self.x1)/self.drawFrames
         self.yStep = (self.y2-self.y1)/self.drawFrames
+ 
+def addToFile(sample):
+    sample = int(sample * (2 ** 15 - 1))
+    f.writeframes(struct.pack("<h", sample))
 
 
 vertices, edges = readObj(objFileName)
-vertices = [rotateVertex(vertex, initRotation) for vertex in vertices]
-lineArray = []
-
-
-leftChannel = []
-rightChannel = []
-for m in range(rotations):
-    lineArray = []
-    for edge in edges:
-        lineArray.append(Line(vertices[edge[0]][0],vertices[edge[0]][1], vertices[edge[1]][0], vertices[edge[1]][1]))
-    for _ in range(pausePerRotate):
-        for line in (lineArray):
-            for i in range(line.drawFrames):
-                rightChannel.append(line.x1+(line.xStep*i))
-                leftChannel.append(line.y1+(line.yStep*i))
-    
-    vertices = [rotateVertex(vertex, rotationArray) for vertex in vertices]
- 
+vertices = rotateVerticies(vertices, initRotation)
 
 with wave.open(saveFileName+".wav", "w") as f:
     f.setnchannels(2)
     f.setsampwidth(2)
     f.setframerate(frameRate)
-    for samples in zip(leftChannel, rightChannel):
-        for sample in samples:
-            sample = int(sample * (2 ** 15 - 1))
-            f.writeframes(struct.pack("<h", sample))
+    for _ in range(rotations):
+        lineArray = []
+        for edge in edges:
+            lineArray.append(Line(vertices[edge[0]][0],vertices[edge[0]][1], vertices[edge[1]][0], vertices[edge[1]][1]))
+        for _ in range(pausePerRotate):
+            for line in lineArray:
+                for i in range(line.drawFrames):
+                    addToFile(line.x1+(line.xStep*i))
+                    addToFile(line.y1+(line.yStep*i))
+        vertices = rotateVerticies(vertices, rotationArray)
